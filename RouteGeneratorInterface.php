2f3ac01ab6b91d5a51f1aad59aad289327ad778b
Daniel Leeb <dl@loremipsum.at>
2019-02-20 16:26:34 +0100

    Add phpdoc for RouteGeneratorInterface

--- RouteGeneratorInterface.php
+++ RouteGeneratorInterface.php
@@ -2,8 +2,17 @@
 
 namespace LoremIpsum\RouteGeneratorBundle;
 
+use LoremIpsum\RouteGeneratorBundle\Exception\MissingRouteHandlerException;
+
 interface RouteGeneratorInterface
 {
+    /**
+     * @param mixed       $value
+     * @param string|null $view
+     * @param array       $context
+     * @return mixed
+     * @throws MissingRouteHandlerException
+     */
     public function generate($value, $view = null, $context = []);
 
     public function addRouteHandler(RouteHandlerInterface $handler);
